4466461b5c2d33e92bf8c77891f35ca13175efe7
agmas <Maxlion360@gmail.com>
2022-06-24 19:44:53 +0400

    a

--- src/main/java/agmass/agmassminipowers/AgmassMiniPowers.java
+++ src/main/java/agmass/agmassminipowers/AgmassMiniPowers.java
@@ -1,9 +1,9 @@
 package agmass.agmassminipowers;
 
-import org.bukkit.Bukkit;
-import org.bukkit.NamespacedKey;
-import org.bukkit.Sound;
-import org.bukkit.SoundCategory;
+import org.bukkit.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -13,8 +13,8 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.block.BlockReceiveGameEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import org.bukkit.persistence.PersistentDataType;
@@ -22,29 +22,33 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.io.File;
-
 public final class AgmassMiniPowers extends JavaPlugin {
 
     public static boolean hasPP(String ppe, Player p) {
-        NamespacedKey key = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "pp");
-        boolean b = p.getPersistentDataContainer().get(key, PersistentDataType.STRING).equals(ppe);
+        NamespacedKey key1 = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "pp");
+        boolean b = p.getPersistentDataContainer().get(key1, PersistentDataType.STRING).equals(ppe);
         return b;
     }
-    public static void wardenTimerReset(Player p, Double t) {
-        for (Player all : Bukkit.getOnlinePlayers()) {
-            if (AgmassMiniPowers.hasPP("warden", all)) {
-                all.showPlayer(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), p);
-                NamespacedKey key = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "wTime");
-                p.getPersistentDataContainer().set(key, PersistentDataType.DOUBLE, t);
-            }
-        }
-    }
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        this.getCommand("resetPP").setExecutor(new CommandKit());
         this.getLogger().info("You're currently running AMP version " + this.getDescription().getVersion() + " (Made for MC" + this.getDescription().getAPIVersion() + ")");
+        MyTask t1 = new MyTask();
+        t1.runTaskTimer(getPlugin(this.getClass()), 0, 1);
+        MyTaske t2 = new MyTaske();
+        t2.runTaskTimer(getPlugin(this.getClass()), 0, 20);
+        getServer().getPluginManager().registerEvents(new MyListener(), this);
+        if (Bukkit.getWorld("pickingWorld") == null) {
+            WorldCreator wc = new WorldCreator("pickingWorld");
 
+            wc.environment(World.Environment.NORMAL);
+            wc.type(WorldType.FLAT);
+            wc.generatorSettings("0;0;0;");
+
+            wc.createWorld();
+        }
     }
 
     @Override
@@ -53,31 +57,87 @@ public final class AgmassMiniPowers extends JavaPlugin {
     }
 }
 
+class CommandKit implements CommandExecutor {
+
+    // This method is called, when somebody uses our command
+    @Override
+    public boolean onCommand(CommandSender sender, Command  command, String label, String[] args) {
+        NamespacedKey key1 = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "pp");
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            player.getPersistentDataContainer().set(key1, PersistentDataType.STRING, "NaN");
+        }
+        return true;
+    }
+}
 
 class MyListener implements Listener {
+    public static void wardenTimerReset(Player p, Double t) {
+        NamespacedKey key = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "wTime");
+        p.getPersistentDataContainer().set(key, PersistentDataType.DOUBLE, t);
+        for (Player all : Bukkit.getOnlinePlayers()) {
+            if (AgmassMiniPowers.hasPP("warden", all)) {
+                all.showPlayer(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), p);
+            }
+        }
+    }
+
     @EventHandler
     public void playerMoveEvent(PlayerMoveEvent event) {
         if (!event.getPlayer().isSneaking()) {
-            AgmassMiniPowers.wardenTimerReset(event.getPlayer(), 30.0);
+            NamespacedKey key = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "wTime");
+            event.getPlayer().getPersistentDataContainer().set(key, PersistentDataType.DOUBLE, 30.0);
+            for (Player all : Bukkit.getOnlinePlayers()) {
+                if (AgmassMiniPowers.hasPP("warden", all)) {
+                    all.showPlayer(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), event.getPlayer());
+                }
+            }
+            wardenTimerReset(event.getPlayer(), 30.0);
         }
     }
 
     @EventHandler
     public void noise2(BlockPlaceEvent event) {
-        AgmassMiniPowers.wardenTimerReset(event.getPlayer(), 30.0);
+        NamespacedKey key = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "wTime");
+        event.getPlayer().getPersistentDataContainer().set(key, PersistentDataType.DOUBLE, 30.0);
+        for (Player all : Bukkit.getOnlinePlayers()) {
+            if (AgmassMiniPowers.hasPP("warden", all)) {
+                all.showPlayer(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), event.getPlayer());
+            }
+        }
     }
 
     @EventHandler
     public void noise5(EntityDamageByEntityEvent event) {
         if (event.getDamager().getType().equals(EntityType.PLAYER)) {
-            AgmassMiniPowers.wardenTimerReset(Bukkit.getServer().getPlayer(event.getDamager().getName()), 30.0);
+            NamespacedKey key = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "wTime");
+            event.getDamager().getPersistentDataContainer().set(key, PersistentDataType.DOUBLE, 30.0);
+            for (Player all : Bukkit.getOnlinePlayers()) {
+                if (AgmassMiniPowers.hasPP("warden", all)) {
+                    all.showPlayer(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), Bukkit.getServer().getPlayer(event.getDamager().getName()));
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void rst(PlayerJoinEvent event) {
+        NamespacedKey key1 = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "pp");
+        if (event.getPlayer().getPersistentDataContainer().get(key1, PersistentDataType.STRING) == null) {
+            event.getPlayer().getPersistentDataContainer().set(key1, PersistentDataType.STRING, "warden");
+        }
+        NamespacedKey key = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "wTime");
+        event.getPlayer().getPersistentDataContainer().set(key, PersistentDataType.DOUBLE, 30.0);
+        Bukkit.broadcastMessage(event.getPlayer().getPersistentDataContainer().get(key, PersistentDataType.DOUBLE).toString());
+        for (Player all : Bukkit.getOnlinePlayers()) {
+            if (AgmassMiniPowers.hasPP("warden", all)) {
+                all.showPlayer(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), event.getPlayer());
+            }
         }
     }
 
     @EventHandler
     public void useAbility(PlayerSwapHandItemsEvent e) {
-        NamespacedKey key = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "pp");
-        e.getPlayer().getPersistentDataContainer().set(key, PersistentDataType.STRING, "warden");
         if (AgmassMiniPowers.hasPP("warden", e.getPlayer())) {
             e.getPlayer().addPotionEffect(PotionEffectType.DARKNESS.createEffect(90, 2));
             for (Entity ps : e.getPlayer().getNearbyEntities(e.getPlayer().getLocation().getX(), e.getPlayer().getLocation().getY(), e.getPlayer().getLocation().getZ())) {
@@ -90,7 +150,13 @@ class MyListener implements Listener {
     }
     @EventHandler
     public void noise3(BlockBreakEvent event) {
-        AgmassMiniPowers.wardenTimerReset(event.getPlayer(), 30.0);
+        NamespacedKey key = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "wTime");
+        event.getPlayer().getPersistentDataContainer().set(key, PersistentDataType.DOUBLE, 30.0);
+        for (Player all : Bukkit.getOnlinePlayers()) {
+            if (AgmassMiniPowers.hasPP("warden", all)) {
+                all.showPlayer(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), event.getPlayer());
+            }
+        }
     }
 
     @EventHandler
@@ -101,7 +167,6 @@ class MyListener implements Listener {
             }
         }
     }
-
     @EventHandler
     public void replaceWarden(EntitySpawnEvent e) {
         if (e.getEntity().getType() == EntityType.WARDEN && Bukkit.getOnlinePlayers().stream().filter((p) -> {

608da4edabbe3dfa1d146fed904493826dfbd770
agmas <Maxlion360@gmail.com>
2022-06-25 23:05:55 +0400

    everything

--- src/main/java/agmass/agmassminipowers/AgmassMiniPowers.java
+++ src/main/java/agmass/agmassminipowers/AgmassMiniPowers.java
@@ -9,11 +9,11 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.block.BlockReceiveGameEvent;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.entity.*;
 import org.bukkit.event.player.*;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
@@ -23,12 +23,14 @@ import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public final class AgmassMiniPowers extends JavaPlugin {
 
     public static List<String> pps = new ArrayList<String>();
+    public static List<Material> beds = new ArrayList<Material>();
     public static boolean hasPP(String ppe, Player p) {
         NamespacedKey key1 = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "pp");
         boolean b = p.getPersistentDataContainer().get(key1, PersistentDataType.STRING).equals(ppe);
@@ -38,8 +40,28 @@ public final class AgmassMiniPowers extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        beds.add(Material.WHITE_BED);
+        beds.add(Material.BLACK_BED);
+        beds.add(Material.BLUE_BED);
+        beds.add(Material.RED_BED);
+        beds.add(Material.BROWN_BED);
+        beds.add(Material.CYAN_BED);
+        beds.add(Material.GREEN_BED);
+        beds.add(Material.LIGHT_BLUE_BED);
+        beds.add(Material.LIGHT_GRAY_BED);
+        beds.add(Material.LIME_BED);
+        beds.add(Material.GRAY_BED);
+        beds.add(Material.MAGENTA_BED);
+        beds.add(Material.ORANGE_BED);
+        beds.add(Material.PINK_BED);
+        beds.add(Material.PURPLE_BED);
+        beds.add(Material.YELLOW_BED);
         pps.add("warden");
-        pps.add("pp2");
+        pps.add("soulling");
+        pps.add("frog");
+        pps.add("elytrian");
+        pps.add("merling");
+        pps.add("human");
         this.getCommand("resetPP").setExecutor(new CommandKit());
         this.getLogger().info("You're currently running AMP version " + this.getDescription().getVersion() + " (Made for MC" + this.getDescription().getAPIVersion() + ")");
         MyTask t1 = new MyTask();
@@ -55,6 +77,14 @@ public final class AgmassMiniPowers extends JavaPlugin {
                 "         \"height\":1\n" +
                 "      }]\n" +
                 "}";
+        String wJs =
+                "{\n" +
+                        "   \"layers\":[\n" +
+                        "      {\n" +
+                        "         \"block\":\"air\",\n" +
+                        "         \"height\":1\n" +
+                        "      }]\n" +
+                        "}";
         if (Bukkit.getWorld("pickingWorld") == null) {
             WorldCreator wc = new WorldCreator("pickingWorld");
 
@@ -63,6 +93,14 @@ public final class AgmassMiniPowers extends JavaPlugin {
             wc.generatorSettings(worldJSON);
 
             wc.createWorld();
+        }
+        if (Bukkit.getWorld("soulWorld") == null) {
+            WorldCreator wce = new WorldCreator("soulWorld");
+            wce.environment(World.Environment.NORMAL);
+            wce.generatorSettings(wJs);
+            wce.generateStructures(false);
+            wce.type(WorldType.FLAT);
+            wce.createWorld();
         }
     }
 
@@ -156,6 +194,15 @@ class MyListener implements Listener {
 
     @EventHandler
     public void useAbility(PlayerSwapHandItemsEvent e) {
+        if (AgmassMiniPowers.hasPP("soulling", e.getPlayer())) {
+            if (e.getPlayer().getWorld().getName() != "soulWorld") {
+                new Location(Bukkit.getWorld("soulWorld"), 0, -1, 0).getBlock().setType(Material.BEDROCK);
+                e.getPlayer().teleport(new Location(Bukkit.getWorld("soulWorld"), 0, 0, 0));
+            } else {
+                e.getPlayer().teleport(Bukkit.getWorld("world").getSpawnLocation());
+                e.getPlayer().sendMessage("You were sent back to the normal world.");
+            }
+        }
         if (AgmassMiniPowers.hasPP("warden", e.getPlayer())) {
             e.getPlayer().addPotionEffect(PotionEffectType.DARKNESS.createEffect(90, 2));
             for (Entity ps : e.getPlayer().getNearbyEntities(e.getPlayer().getLocation().getX(), e.getPlayer().getLocation().getY(), e.getPlayer().getLocation().getZ())) {
@@ -201,6 +248,25 @@ class MyListener implements Listener {
             }
         }
     }
+
+    @EventHandler
+    public void onEntityTarget(EntityTargetEvent event) {
+        if ((event.getTarget() instanceof Player) && (AgmassMiniPowers.hasPP("soulling", Bukkit.getPlayer(event.getTarget().getName())))) {
+            EntityType et = event.getEntity().getType();
+            if (et.equals(EntityType.STRIDER) || et.equals(EntityType.ZOMBIFIED_PIGLIN) || et.equals(EntityType.GHAST) || et.equals(EntityType.PIGLIN) || et.equals(EntityType.HOGLIN) || et.equals(EntityType.MAGMA_CUBE) || et.equals(EntityType.BLAZE) || et.equals(EntityType.WITHER_SKELETON) || et.equals(EntityType.PIGLIN_BRUTE))
+                event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void soulBond(EntityDamageEvent event) {
+        EntityType et = event.getEntity().getType();
+        if (et.equals(EntityType.STRIDER) || et.equals(EntityType.ZOMBIFIED_PIGLIN) || et.equals(EntityType.GHAST) || et.equals(EntityType.PIGLIN) || et.equals(EntityType.HOGLIN) || et.equals(EntityType.MAGMA_CUBE) || et.equals(EntityType.BLAZE) || et.equals(EntityType.WITHER_SKELETON) || et.equals(EntityType.PIGLIN_BRUTE))
+            Bukkit.getOnlinePlayers().stream()
+                    .filter(p -> AgmassMiniPowers.hasPP("soulling", p))
+                    .forEach(p -> p.damage(event.getDamage() / 4));
+    }
+
     @EventHandler
     public void replaceWarden(EntitySpawnEvent e) {
         if (e.getEntity().getType() == EntityType.WARDEN && Bukkit.getOnlinePlayers().stream().filter((p) -> {
@@ -217,7 +283,19 @@ class MyListener implements Listener {
     }
 
     @EventHandler
+    public void death(PlayerDeathEvent e) {
+        e.getDrops().clear();
+    }
+    @EventHandler
     public void rclick(PlayerInteractEvent e) {
+        if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if (AgmassMiniPowers.hasPP("soulling", e.getPlayer())) {
+                if (AgmassMiniPowers.beds.contains(e.getClickedBlock())) {
+                    e.getPlayer().sendMessage("You can't sleep now, You never know what could happen.");
+                    e.setCancelled(true);
+                }
+            }
+        }
         if  (e.getPlayer().getInventory().getItemInMainHand().getItemMeta() != null) {
             if (e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName().equals(ChatColor.LIGHT_PURPLE + "LEFT")) {
                 NamespacedKey key13123 = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "selectedPP");
@@ -271,12 +349,25 @@ class MyTask extends BukkitRunnable {
                 bm.addPage(ChatColor.LIGHT_PURPLE + "Drop this book to choose this origin! [WARDEN]\n" + ChatColor.GRAY + "A blind creature born in the depths of caverns from a strange sculk substance with feared powers." + ChatColor.RED + "\n\nTerrible Vision" + ChatColor.GRAY + "\nYour blind eyes make you only be able to see players making sound.", ChatColor.RED + "Sculk-Infused" + ChatColor.GRAY + "\nYou have constant 'sculk' around your screen, making you mine, walk and jump slower." + ChatColor.GOLD + "\n\nHeartbeat" + ChatColor.GRAY + "\nA heartbeat sound constantly plays around you. can be disabled by setting 'voices/speech' to 0 in sound settings.", ChatColor.GREEN + "Dark Spread" + ChatColor.BLUE + " [F]" + ChatColor.GRAY + "\nUsing the F key, you can give anyone around you the darkness effect for 9 seconds, and inflicts you with it for ~4s." + ChatColor.GREEN + "\n\nSculky Friends" + ChatColor.GRAY + "\nYou will not trigger sculk sensors.", ChatColor.GREEN + "Shrieked" + ChatColor.GRAY + "\nWhen a player activates a shrieker, Instead of a warden spawning, you will be summoned for an easy catch. Warden will not spawn as long as you are online.");
             }
             if (p.getPersistentDataContainer().get(key13123, PersistentDataType.INTEGER) == 1) {
-                bm.addPage(ChatColor.LIGHT_PURPLE + "Drop this book to choose this origin! [SOULLING]\n" + ChatColor.GRAY + "A blind creature born in the depths of caverns from a strange sculk substance with feared powers." + ChatColor.RED + "\n\nTerrible Vision" + ChatColor.GRAY + "\nYour blind eyes make you only be able to see players making sound.", ChatColor.RED + "Sculk-Infused" + ChatColor.GRAY + "\nYou have constant 'sculk' around your screen, making you mine, walk and jump slower." + ChatColor.GOLD + "\n\nHeartbeat" + ChatColor.GRAY + "\nA heartbeat sound constantly plays around you. can be disabled by setting 'voices/speech' to 0 in sound settings.", ChatColor.GREEN + "Dark Spread" + ChatColor.BLUE + " [F]" + ChatColor.GRAY + "\nUsing the F key, you can give anyone around you the darkness effect for 9 seconds, and inflicts you with it for ~4s." + ChatColor.GREEN + "\n\nSculky Friends" + ChatColor.GRAY + "\nYou will not trigger sculk sensors.", ChatColor.GREEN + "Shrieked" + ChatColor.GRAY + "\nWhen a player activates a shrieker, Instead of a warden spawning, you will be summoned for an easy catch. Warden will not spawn as long as you are online.");
+                bm.addPage(ChatColor.LIGHT_PURPLE + "Drop this book to choose this origin! [SOULLING]\n" + ChatColor.GRAY + "Trapped inside the nether's soul sand valleys, souls are trying their best not to go back to their painful origins." + ChatColor.RED + "\nWeak" + ChatColor.GRAY + "\nYou have weakness and mining fatigue" + ChatColor.RED + "\nTrail" + ChatColor.GRAY + "\nYou leave a trail of lava", ChatColor.GOLD + "\n\nGhostly Pockets" + ChatColor.GRAY + "\nYou do not drop your items.", ChatColor.GREEN + "Soul Sand" + ChatColor.BLUE + " [F]" + ChatColor.GRAY + "\nUsing the F key, you can teleport into a new air world. Only you and other soullings can access this." + ChatColor.RED + "\n\nSoul-bound" + ChatColor.GRAY + "\nWhen a nether creature takes damage, you take 1/4 of it's damage.", ChatColor.GREEN + "Neighbours" + ChatColor.GRAY + "\nNether mobs will not target you.");
+            }
+            if (p.getPersistentDataContainer().get(key13123, PersistentDataType.INTEGER) == 2) {
+                bm.addPage(ChatColor.LIGHT_PURPLE + "Drop this book to choose this origin! [FROG]\n" + ChatColor.GRAY + "Ribbit" + ChatColor.RED + "\nHeavy Eater" + ChatColor.GRAY + "\nWhen damaging a mob or player, you get hunger. \nYou cannot get natural hunger" + ChatColor.RED + "\nTrail" + ChatColor.GRAY + "\nYou leave a trail of lava", ChatColor.GOLD + "\n\nGhostly Pockets" + ChatColor.GRAY + "\nYou do not drop your items.", ChatColor.GREEN + "Soul Sand" + ChatColor.BLUE + " [F]" + ChatColor.GRAY + "\nUsing the F key, you can teleport into a new air world. Only you and other soullings can access this." + ChatColor.RED + "\n\nSoul-bound" + ChatColor.GRAY + "\nWhen a nether creature takes damage, you take 1/4 of it's damage.", ChatColor.GREEN + "Neighbours" + ChatColor.GRAY + "\nNether mobs will not target you.");
+            }
+            if (p.getPersistentDataContainer().get(key13123, PersistentDataType.INTEGER) == AgmassMiniPowers.pps.size() - 1) {
+                bm.addPage(ChatColor.LIGHT_PURPLE + "Drop this book to choose this origin! [HUMAN]\n" + ChatColor.GRAY + "Dude. It's normal minecraft.");
             }
             book.setItemMeta(bm);
             inv.setItem(4, book);
             inv.setItem(5, r);
             inv.setItem(3, l);
+        });
+        Bukkit.getOnlinePlayers().stream().filter((p) -> {
+            return AgmassMiniPowers.hasPP("soulling", p);
+        }).forEach((p) -> {
+            p.addPotionEffect(PotionEffectType.WEAKNESS.createEffect(2, 0));
+            p.addPotionEffect(PotionEffectType.SLOW_DIGGING.createEffect(2, 0));
+            p.getWorld().spawnParticle(Particle.DRIP_LAVA, p.getLocation(), 1);
         });
         for(Player all : Bukkit.getOnlinePlayers()) {
             NamespacedKey key = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "wTime");
@@ -287,6 +378,7 @@ class MyTask extends BukkitRunnable {
                         .forEach(p -> p.hidePlayer(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), all));
             }
         }
+
         Bukkit.getOnlinePlayers().stream()
                 .filter(p -> AgmassMiniPowers.hasPP("warden", p))
                 .forEach(p -> p.setFreezeTicks(40));

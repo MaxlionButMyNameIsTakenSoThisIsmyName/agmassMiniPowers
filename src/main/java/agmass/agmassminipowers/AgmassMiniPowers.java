9f83cc7ec9f1534f0982ba29f457ee32d1917236
agmas <56167347+MaxlionButMyNameIsTakenSoThisIsmyName@users.noreply.github.com>
2022-06-25 11:02:12 +0400

    ynnnnnnnnnnnnnnnnnnnnnn pp poopoo

--- src/main/java/agmass/agmassminipowers/AgmassMiniPowers.java
+++ src/main/java/agmass/agmassminipowers/AgmassMiniPowers.java
@@ -4,9 +4,9 @@ import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,6 +15,7 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.block.BlockReceiveGameEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
@@ -22,14 +23,17 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.material.MaterialData;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.List;
+import java.util.Locale;
+
 public final class AgmassMiniPowers extends JavaPlugin {
 
+    public static List<String> pps;
     public static boolean hasPP(String ppe, Player p) {
         NamespacedKey key1 = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "pp");
         boolean b = p.getPersistentDataContainer().get(key1, PersistentDataType.STRING).equals(ppe);
@@ -39,6 +43,8 @@ public final class AgmassMiniPowers extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        pps.add("warden");
+        pps.add("pp2");
         this.getCommand("resetPP").setExecutor(new CommandKit());
         this.getLogger().info("You're currently running AMP version " + this.getDescription().getVersion() + " (Made for MC" + this.getDescription().getAPIVersion() + ")");
         MyTask t1 = new MyTask();
@@ -138,7 +144,7 @@ class MyListener implements Listener {
     public void rst(PlayerJoinEvent event) {
         NamespacedKey key1 = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "pp");
         NamespacedKey key13123 = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "selectedPP");
-        event.getPlayer().getPersistentDataContainer().set(key13123, PersistentDataType.STRING, "warden");
+        event.getPlayer().getPersistentDataContainer().set(key13123, PersistentDataType.INTEGER, 0);
         if (event.getPlayer().getPersistentDataContainer().get(key1, PersistentDataType.STRING) == null) {
             event.getPlayer().getPersistentDataContainer().set(key1, PersistentDataType.STRING, "NaN");
         }
@@ -197,6 +203,22 @@ class MyListener implements Listener {
         }
 
     }
+
+    @EventHandler
+    public void rclick(PlayerInteractEvent e) {
+        if(e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName().equals(ChatColor.LIGHT_PURPLE + "LEFT")) {
+            NamespacedKey key13123 = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "selectedPP");
+            if (e.getPlayer().getPersistentDataContainer().get(key13123, PersistentDataType.INTEGER) > 0) {
+                e.getPlayer().getPersistentDataContainer().set(key13123, PersistentDataType.INTEGER, e.getPlayer().getPersistentDataContainer().get(key13123, PersistentDataType.INTEGER) - 1);
+            }
+        }
+        if(e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName().equals(ChatColor.LIGHT_PURPLE + "RIGHT")) {
+            NamespacedKey key13123 = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "selectedPP");
+            if (e.getPlayer().getPersistentDataContainer().get(key13123, PersistentDataType.INTEGER) != AgmassMiniPowers.pps.size()) {
+                e.getPlayer().getPersistentDataContainer().set(key13123, PersistentDataType.INTEGER, e.getPlayer().getPersistentDataContainer().get(key13123, PersistentDataType.INTEGER) + 1);
+            }
+        }
+    }
 }
 
 
@@ -209,22 +231,38 @@ class MyTask extends BukkitRunnable {
             return AgmassMiniPowers.hasPP("NaN", p);
         }).forEach((p) -> {
             p.teleport(location);
-            p.
+            p.addPotionEffect(PotionEffectType.INVISIBILITY.createEffect(2, 255));
+            p.addPotionEffect(PotionEffectType.DAMAGE_RESISTANCE.createEffect(2, 255));
             PlayerInventory inv = p.getInventory();
             inv.clear();
+            ItemStack l = new ItemStack(Material.SPECTRAL_ARROW, 1);
+            ItemMeta lm = l.getItemMeta();
+            lm.setDisplayName(ChatColor.LIGHT_PURPLE + "LEFT");
+            lm.addEnchant(Enchantment.SWEEPING_EDGE, 8,true);
+            l.setItemMeta(lm);
+            ItemStack r = new ItemStack(Material.SPECTRAL_ARROW, 1);
+            ItemMeta rm = r.getItemMeta();
+            rm.setDisplayName(ChatColor.LIGHT_PURPLE + "RIGHT");
+            rm.addEnchant(Enchantment.SWEEPING_EDGE, 8,true);
+            r.setItemMeta(rm);
             ItemStack book = new ItemStack(Material.WRITTEN_BOOK, 1);
             BookMeta bm = (BookMeta) book.getItemMeta();
             NamespacedKey key13123 = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "selectedPP");
-            if (p.getPersistentDataContainer().get(key13123, PersistentDataType.STRING) == null) {
-                p.getPersistentDataContainer().set(key13123, PersistentDataType.STRING, "warden");
+            if (p.getPersistentDataContainer().get(key13123, PersistentDataType.INTEGER) == null) {
+                    p.getPersistentDataContainer().set(key13123, PersistentDataType.INTEGER, 0);
             }
-            bm.setTitle(ChatColor.GREEN + p.getPersistentDataContainer().get(key13123, PersistentDataType.STRING).toUpperCase());
+            bm.setTitle(ChatColor.GREEN + AgmassMiniPowers.pps.get(p.getPersistentDataContainer().get(key13123, PersistentDataType.INTEGER)).toUpperCase());
             bm.setAuthor(ChatColor.BLUE + "AMP Plugin");
-            if (p.getPersistentDataContainer().get(key13123, PersistentDataType.STRING) == "warden") {
+            if (p.getPersistentDataContainer().get(key13123, PersistentDataType.INTEGER) == 0) {
                 bm.addPage(ChatColor.LIGHT_PURPLE + "Drop this book to choose this origin! [WARDEN]\n" + ChatColor.GRAY + "A blind creature born in the depths of caverns from a strange sculk substance with feared powers." + ChatColor.RED + "\n\nTerrible Vision" + ChatColor.GRAY + "\nYour blind eyes make you only be able to see players making sound.", ChatColor.RED + "Sculk-Infused" + ChatColor.GRAY + "\nYou have constant 'sculk' around your screen, making you mine, walk and jump slower." + ChatColor.GOLD + "\n\nHeartbeat" + ChatColor.GRAY + "\nA heartbeat sound constantly plays around you. can be disabled by setting 'voices/speech' to 0 in sound settings.", ChatColor.GREEN + "Dark Spread" + ChatColor.BLUE + " [F]" + ChatColor.GRAY + "\nUsing the F key, you can give anyone around you the darkness effect for 9 seconds, and inflicts you with it for ~4s." + ChatColor.GREEN + "\n\nSculky Friends" + ChatColor.GRAY + "\nYou will not trigger sculk sensors.", ChatColor.GREEN + "Shrieked" + ChatColor.GRAY + "\nWhen a player activates a shrieker, Instead of a warden spawning, you will be summoned for an easy catch. Warden will not spawn as long as you are online.");
+            }
+            if (p.getPersistentDataContainer().get(key13123, PersistentDataType.INTEGER) == 0) {
+                bm.addPage(ChatColor.LIGHT_PURPLE + "Drop this book to choose this origin! [SOULLING]\n" + ChatColor.GRAY + "A blind creature born in the depths of caverns from a strange sculk substance with feared powers." + ChatColor.RED + "\n\nTerrible Vision" + ChatColor.GRAY + "\nYour blind eyes make you only be able to see players making sound.", ChatColor.RED + "Sculk-Infused" + ChatColor.GRAY + "\nYou have constant 'sculk' around your screen, making you mine, walk and jump slower." + ChatColor.GOLD + "\n\nHeartbeat" + ChatColor.GRAY + "\nA heartbeat sound constantly plays around you. can be disabled by setting 'voices/speech' to 0 in sound settings.", ChatColor.GREEN + "Dark Spread" + ChatColor.BLUE + " [F]" + ChatColor.GRAY + "\nUsing the F key, you can give anyone around you the darkness effect for 9 seconds, and inflicts you with it for ~4s." + ChatColor.GREEN + "\n\nSculky Friends" + ChatColor.GRAY + "\nYou will not trigger sculk sensors.", ChatColor.GREEN + "Shrieked" + ChatColor.GRAY + "\nWhen a player activates a shrieker, Instead of a warden spawning, you will be summoned for an easy catch. Warden will not spawn as long as you are online.");
             }
             book.setItemMeta(bm);
             inv.setItem(4, book);
+            inv.setItem(5, r);
+            inv.setItem(3, l);
         });
         for(Player all : Bukkit.getOnlinePlayers()) {
             NamespacedKey key = new NamespacedKey(AgmassMiniPowers.getPlugin(AgmassMiniPowers.class), "wTime");
